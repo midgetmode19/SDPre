70e846fcaf942d68e91deb76ec29e456332c3e98
Samantha Janik <samajanik@gmail.com>
2018-06-23 15:55:49 -0600

    exercise 4

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,6 @@
+public class PrintingChoicesEscapes {
+  public static void main( String [] args ) {
+    System.out.print("Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nI am learning java the hard way!\n");
+
+  }
+}
