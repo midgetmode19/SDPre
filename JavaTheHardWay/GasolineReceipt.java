3d5ac7ab853cedda453096b327c83f2dea53ce11
Samantha Janik <samajanik@gmail.com>
2018-06-23 15:05:47 -0600

    exercise 2

--- JavaTheHardWay/GasolineReceipt.java
+++ JavaTheHardWay/GasolineReceipt.java
@@ -7,9 +7,9 @@ public class GasolineReceipt {
     System.out.println( "| 2015-03-29 04:38PM     |" );
     System.out.println( "|                        |" );
     System.out.println( "| Gallons:       10.870  |" );
-    System.out.println( "| Price/gallon: $ 2.089  |" );
+    System.out.println( "| Price/gallon: $ 3.089  |" );
     System.out.println( "|                        |" );
-    System.out.println( "| Fuel total:   $ 22.71  |" );
+    System.out.println( "| Fuel total:   $ 33.58  |" );
     System.out.println( "|                        |" );
     System.out.println( "+------------------------+" );
   }
