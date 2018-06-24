e62ab955935283e87a1302ed3f6e7bc2e4ebaf01
Samantha Janik <samajanik@gmail.com>
2018-06-23 20:12:09 -0600

    Completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Samantha Janik";
+    int age = 30;
+    String state = "Colorado";
+
+    System.out.println("\nName: " + name);
+    System.out.println("Age " + age);
+    System.out.println("State: " + state + "\n");
+  }
+}
