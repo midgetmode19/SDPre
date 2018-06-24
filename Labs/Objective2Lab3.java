44677213c6372d064aaa5e9b7ef96a3f4de4c616
Samantha Janik <samajanik@gmail.com>
2018-06-23 20:21:20 -0600

    Completed Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+    food1 = "Watermelon";
+    food2 = "Chocolate";
+    food3 = "anything with sugar if I'm honest";
+
+    System.out.println("\nMy favorite foods are: " + food1 + ", " + food2 + ", and " + food3 + ".\n");
+
+  }
+}
