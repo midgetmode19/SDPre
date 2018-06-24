ecc455ae97af3130816db63562eb8fd22934d0a6
Samantha Janik <samajanik@gmail.com>
2018-06-24 00:30:21 -0600

    Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Objective3Lab1{
+  public static void main(String[] args){
+    String userInput = "";
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Please type a word:");
+    userInput = input.next();
+    System.out.println("You entered the word " + userInput + ".");
+    System.out.println();
+    }
+}
