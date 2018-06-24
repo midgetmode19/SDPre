7c022ade1458e89423595ea5a98b9c551e508e2c
Samantha Janik <samajanik@gmail.com>
2018-06-23 20:34:45 -0600

    Completed Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+    num1 = 7;
+    num2 = 13;
+    num3 = 76;
+    average = (num1+num2+num3)/3;
+
+    System.out.println("\nThe Average of 7+13+76 is " + average + "\n");
+
+  }
+}
