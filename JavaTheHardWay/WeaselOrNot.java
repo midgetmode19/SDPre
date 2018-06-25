3195da9065edae9e33b34340e3ec32deaa1a0696
Samantha Janik <samajanik@gmail.com>
2018-06-24 19:14:12 -0600

    Exercise 13

---
+++ JavaTheHardWay/WeaselOrNot.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WeaselOrNot{
+  public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    String word;
+    boolean yep, nope;
+
+    System.out.println("\nType the word \"weasel\" please.");
+    word = keyboard.next();
+    yep = "weasel".equals(word); //This works... WHY??
+    nope = ! word.equals("weasel");
+
+    System.out.println("You typed what was requested: " + yep);
+    System.out.println("You ignored polite instructions: " + nope);
+  }
+}
